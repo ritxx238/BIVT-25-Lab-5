9f1af848f7f3632622eae3ba1724f4044f6db167
Akum6x <dfrale@mail.ru>
2025-11-16 16:57:00 +0300

    Update White.cs

--- Lab5/White.cs
+++ Lab5/White.cs
@@ -1,4 +1,4 @@
-﻿using System.Linq;
+using System.Linq;
 using System.Runtime.InteropServices;
 
 namespace Lab5
@@ -10,7 +10,24 @@ namespace Lab5
             double average = 0;
 
             // code here
+            double rowCount = matrix.GetLength(0); // кол-во строк
+            double colCount = matrix.GetLength(1); // кол-во столбцов
 
+            double count = 0;
+            double sum = 0;
+            // проход по элементам матрицы
+            for (int i = 0; i < rowCount; i++)
+            {
+                for (int  j = 0; j < colCount; j++)
+                {
+                    if (matrix[i, j] > 0)
+                    {
+                        count+=1;
+                        sum += matrix[i, j];
+                    }
+                }
+            }
+            average = sum / count;
             // end
 
             return average;
@@ -20,7 +37,22 @@ namespace Lab5
             int row = 0, col = 0;
 
             // code here
+            int rowCount = matrix.GetLength(0);
+            int colCount = matrix.GetLength(1);
+            int min = matrix[0, 0];
 
+            for (int i = 0; i < rowCount; i++)
+            {
+                for (int j = 0; j < colCount; j++)
+                {
+                    if (matrix[i, j] < min)
+                    {
+                        min = matrix[i, j];
+                        row = i;
+                        col = j;
+                    }
+                }
+            }
             // end
 
             return (row, col);
@@ -29,7 +61,29 @@ namespace Lab5
         {
 
             // code here
+            int rowCount = matrix.GetLength(0);
+            int colCount = matrix.GetLength(1);
 
+            if (k < 0 || k >= colCount) return;
+
+            int maxRow = 0;
+            int max = matrix[0, k];
+
+            for (int i = 0; i < rowCount; i++)
+            {
+                if (matrix[i, k] > max)
+                {
+                    max = matrix[i, k];
+                    maxRow = i;
+                }
+            }
+
+            for (int j = 0; j < colCount; j++)
+            {
+                int temp = matrix[maxRow, j];
+                matrix[maxRow, j] = matrix[0, j];
+                matrix[0, j] = temp;
+            }
             // end
 
         }
@@ -38,7 +92,33 @@ namespace Lab5
             int[,] answer = null;
 
             // code here
+            int rowCount = matrix.GetLength(0);
+            int colCount = matrix.GetLength(1);
 
+            if (rowCount == 1) return new int[0, colCount];
+
+            int minRow = 0;
+            int min = matrix[0, 0];
+
+            for (int i = 1; i < rowCount; i++)
+            {
+                if (matrix[i, 0] < min)
+                {
+                    min = matrix[i, 0];
+                    minRow = i;
+                }
+            }
+
+            answer = new int[rowCount - 1, colCount];
+            for (int i = 0, newRow = 0; i < rowCount; i++)
+            {
+                if (i != minRow)
+                {
+                    for (int j = 0; j < colCount; j++)
+                        answer[newRow, j] = matrix[i, j];
+                    newRow++;
+                }
+            }
             // end
 
             return answer;
@@ -48,7 +128,12 @@ namespace Lab5
             int sum = 0;
 
             // code here
+            int size = matrix.GetLength(0);
+            // проверяем что матрица квадратная
+            if (size != matrix.GetLength(1)) return 0;
 
+            for (int i = 0; i < size; i++)
+                sum += matrix[i, i];
             // end
 
             return sum;
@@ -57,7 +142,36 @@ namespace Lab5
         {
 
             // code here
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                int maxIndex = -1, lastNegative = -1;
+                int maxVal = int.MinValue;
+                bool foundNegative = false;
 
+                // Поиск максимального до первого отрицательного и последнего отрицательного
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    if (!foundNegative && matrix[i, j] < 0)
+                        foundNegative = true;
+
+                    if (!foundNegative && matrix[i, j] > maxVal)
+                    {
+                        maxVal = matrix[i, j];
+                        maxIndex = j;
+                    }
+
+                    if (matrix[i, j] < 0)
+                        lastNegative = j;
+                }
+
+                // Обмен
+                if (maxIndex != -1 && lastNegative != -1)
+                {
+                    int temp = matrix[i, maxIndex];
+                    matrix[i, maxIndex] = matrix[i, lastNegative];
+                    matrix[i, lastNegative] = temp;
+                }
+            }
             // end
 
         }
@@ -66,7 +180,17 @@ namespace Lab5
             int[] negatives = null;
 
             // code here
+            List<int> negativeList = new List<int>();
 
+            for (int i = 0; i < matrix.GetLength(0); i++)
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                    if (matrix[i, j] < 0)
+                        negativeList.Add(matrix[i, j]);
+
+            if (negativeList.Count == 0)
+                negatives = null;
+            else
+                negatives = negativeList.ToArray();
             // end
 
             return negatives;
@@ -75,7 +199,27 @@ namespace Lab5
         {
 
             // code here
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                if (matrix.GetLength(1) == 1) continue;
 
+                int maxIndex = 0;
+                for (int j = 1; j < matrix.GetLength(1); j++)
+                    if (matrix[i, j] > matrix[i, maxIndex])
+                        maxIndex = j;
+
+                if (maxIndex > 0 && maxIndex < matrix.GetLength(1) - 1)
+                {
+                    if (matrix[i, maxIndex - 1] <= matrix[i, maxIndex + 1])
+                        matrix[i, maxIndex - 1] *= 2;
+                    else
+                        matrix[i, maxIndex + 1] *= 2;
+                }
+                else if (maxIndex == 0)
+                    matrix[i, 1] *= 2;
+                else
+                    matrix[i, maxIndex - 1] *= 2;
+            }
             // end
 
         }
@@ -83,7 +227,15 @@ namespace Lab5
         {
 
             // code here
-
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1) / 2; j++)
+                {
+                    int temp = matrix[i, j];
+                    matrix[i, j] = matrix[i, matrix.GetLength(1) - 1 - j];
+                    matrix[i, matrix.GetLength(1) - 1 - j] = temp;
+                }
+            }
             // end
 
         }
@@ -91,7 +243,19 @@ namespace Lab5
         {
 
             // code here
+            int n = matrix.GetLength(0);
+            if (n != matrix.GetLength(1)) return; // проверка что матрица квадратная
+            if (n == 0) return; // проверка на пустую матрицу
 
+            int startRow = n / 2;
+
+            for (int i = startRow; i < n; i++)
+            {
+                for (int j = 0; j <= i; j++) // j <= i всегда в пределах т.к. i < n
+                {
+                    matrix[i, j] = 1;
+                }
+            }
             // end
 
         }
@@ -100,7 +264,38 @@ namespace Lab5
             int[,] answer = null;
 
             // code here
+            List<int[]> rowsWithoutZeros = new List<int[]>();
+            int colCount = matrix.GetLength(1);
 
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                bool hasZero = false;
+                for (int j = 0; j < colCount; j++)
+                {
+                    if (matrix[i, j] == 0)
+                    {
+                        hasZero = true;
+                        break;
+                    }
+                }
+                if (!hasZero)
+                {
+                    int[] row = new int[colCount];
+                    for (int j = 0; j < colCount; j++)
+                        row[j] = matrix[i, j];
+                    rowsWithoutZeros.Add(row);
+                }
+            }
+
+            if (rowsWithoutZeros.Count == 0)
+                answer = new int[0, colCount];
+            else
+            {
+                answer = new int[rowsWithoutZeros.Count, colCount];
+                for (int i = 0; i < rowsWithoutZeros.Count; i++)
+                    for (int j = 0; j < colCount; j++)
+                        answer[i, j] = rowsWithoutZeros[i][j];
+            }
             // end
 
             return answer;
@@ -109,7 +304,7 @@ namespace Lab5
         {
 
             // code here
-
+            Array.Sort(array, (a, b) => a.Sum().CompareTo(b.Sum()));
             // end
 
         }
